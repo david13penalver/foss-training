5e4028e6d57bfc9664a8f5b61ec798ac4a1bc3f3
David Peñalver <david13penalver@gmail.com>
2025-12-31 16:10:57 +0100

    Added Spring notations to exclude JPA until its necessary

--- foss-training-api/src/main/java/com/david13penalver/foss_training_api/FossTrainingApiApplication.java
+++ foss-training-api/src/main/java/com/david13penalver/foss_training_api/FossTrainingApiApplication.java
@@ -2,8 +2,15 @@ package com.david13penalver.foss_training_api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.hibernate.autoconfigure.HibernateJpaAutoConfiguration;
+import org.springframework.boot.jdbc.autoconfigure.DataSourceAutoConfiguration;
+import org.springframework.boot.jdbc.autoconfigure.DataSourceTransactionManagerAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {
+		DataSourceAutoConfiguration.class,
+		DataSourceTransactionManagerAutoConfiguration.class,
+		HibernateJpaAutoConfiguration.class
+})
 public class FossTrainingApiApplication {
 
 	public static void main(String[] args) {
